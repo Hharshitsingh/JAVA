ef6ed98262031478745ddbe44eedbb129918fc3c
Hharshitsingh <realharshitsingh@gmail.com>
2020-07-01 14:46:46 -0700

    update

--- JAVA Programe/1.Hello World.java
+++ JAVA Programe/1.Hello World.java
@@ -1,6 +1,5 @@
 class HelloWorld{
     public static void main(String []args) {
-        System.out.print("Hello, World!\n");
-        System.out.print("I'M your first java Program");
+        System.out.print("Hello World! \nI'm your first JAVA Program");
     }
 }
