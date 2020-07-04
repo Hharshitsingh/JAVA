0ae48171e2cd89fcca5ee43da1be225c84e804d0
Hharshitsingh <realharsgitsingh@gmail.com>
2020-07-04 15:14:30 +0530

    update

---
+++ JAVA Programe/5.String.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Str{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+        String str1 = str.toLowerCase();
+        System.out.println(str);
+        System.out.println(str1);
+        
+    }
+}
